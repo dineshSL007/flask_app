7006811e32b7939594b5cd5e45bf30dff7156af0
dineshSL007 <30835511+dineshSL007@users.noreply.github.com>
2021-09-02 18:16:05 +0530

    Delete scripts directory

--- scripts/content_management.py
+++
@@ -1,7 +0,0 @@
-def content_management():
-   DICT_TECH = {"Basics":[["Introduction to Python","/IntroPython/"],["variables in python","/Var_in_python/"],["conditions &amp; looping in python","/condition_looping/"],["Multithreading in python","/multithreading/"]],
-                "Web_dev":[["Introduction to Flask framework","/IntroFlask/"],["Connecting to Database","/ConnToDatabase/"]],  
-                "Image_Pro":[["Introduction to OpenCV libray in python","/Intro_OpenCV/"],["Template matching in OpenCV","/Template_OpenCV/"]]
-                }
-   return DICT_TECH
-   
